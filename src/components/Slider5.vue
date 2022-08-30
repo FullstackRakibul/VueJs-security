385c445ae189310bf1f6d65cb266d28c03dc48e6
Rakibul Hasan Rabbi <rakibul.cse.47@gmail.com>
2022-08-30 15:51:38 +0600

    New Design text change

--- src/components/Slider5.vue
+++ src/components/Slider5.vue
@@ -4,9 +4,9 @@
     <div class="flex flex-col justify-center align-middle items-center bg-[url('@/assets/img/security-15.gif')] w-full h-full">
       <div @mousemove="onMousemove" :style="{ backgroundColor: `hsl(${x}, 80%, 50%)` }"
            class="head movearea rounded-md p-3 mx-3">
-        <h2 class="text-4xl font-semibold text-center p-3">Give your computer a rest when you're not at your desk.</h2>
+        <h2 class="text-4xl text-white font-semibold text-center p-3">Give your computer a rest when you're not at your desk.</h2>
 
-        <p class="text-center font-light font-mono p-2 ">Passwords are like dirty socks. If left laying around, they'll create a stinky mess.</p>
+        <p class="text-center font-light font-mono p-2 text-white ">Passwords are like dirty socks. If left laying around, they'll create a stinky mess.</p>
       </div>
       <div class="mx-2 p-4">
         <img src="../assets/img/security-11.gif">
